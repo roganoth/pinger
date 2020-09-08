6058829b9ea9b254a975c409a6a495d28efa3436
dan demeo <chronos937@gmail.com>
2020-09-08 09:45:26 -0400

    setting up database

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,25 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "lists",
-});
+// var connection;
+//  = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "lists",
+// });
 
 // if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 // } else {
-//   conection = mysql.createConnection({
+//   var connection = mysql.createConnection({
 //     host: "localhost",
 //     port: 3306,
 //     user: "root",
 //     password: "password",
 //     database: "lists",
 //   });
-// }
+
 connection.connect(function (err) {
   if (err) {
     console.error("error connection: " + err.stack);
