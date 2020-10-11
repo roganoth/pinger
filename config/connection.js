471458efd36b928e9466e453c6d46d6a9f6709e3
dan demeo <chronos937@gmail.com>
2020-10-10 23:53:21 -0400

    replaced missing '}' and deleted old code in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-// var connection;
+var connection;
 //  = mysql.createConnection({
 //   host: "localhost",
 //   port: 3306,
@@ -9,16 +9,17 @@ const mysql = require("mysql");
 //   database: "lists",
 // });
 
-// if (process.env.JAWSDB_URL) {
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "lists",
-//   });
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "lists",
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
